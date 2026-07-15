78d7d45df3a9ae3acd9dc1398096cb3ce2b3a998
Konstantin Okunev <kokunev@picarro.com>
2026-07-15 10:01:24 -0700

    # Cleanup - removed DEBUG nets

--- project_1/project_1.runs/impl_1/design_1_wrapper.tcl
+++ project_1/project_1.runs/impl_1/design_1_wrapper.tcl
@@ -97,6 +97,8 @@ proc step_failed { step } {
 OPTRACE "impl_1" END { }
 }
 
+set_msg_config -id {Synth 8-256} -limit 10000
+set_msg_config -id {Synth 8-638} -limit 10000
 
 OPTRACE "impl_1" START { ROLLUP_1 }
 OPTRACE "Phase: Init Design" START { ROLLUP_AUTO }
@@ -104,8 +106,13 @@ start_step init_design
 set ACTIVE_STEP init_design
 set rc [catch {
   create_msg_db init_design.pb
+  set_param checkpoint.writeSynthRtdsInDcp 1
   set_param chipscope.maxJobs 5
+  set_param xicom.use_bs_reader 1
+  set_param tcl.collectionResultDisplayLimit 0
   set_param general.usePosixSpawnForFork 1
+  set_param bd.open.in_stealth_mode 9
+  set_param synth.incrementalSynthesisCache ./.Xil/Vivado-759351-emerald/incrSyn
   set_param runs.launchOptions { -jobs 10  }
 OPTRACE "create in-memory project" START { }
   create_project -in_memory -part xc7a100tfgg484-2
--- project_1/project_1.runs/synth_1/design_1_wrapper.tcl
+++ project_1/project_1.runs/synth_1/design_1_wrapper.tcl
@@ -56,7 +56,15 @@ if {$::dispatch::connected} {
 }
 
 OPTRACE "synth_1" START { ROLLUP_AUTO }
+set_param checkpoint.writeSynthRtdsInDcp 1
+set_param chipscope.maxJobs 5
+set_param xicom.use_bs_reader 1
+set_param tcl.collectionResultDisplayLimit 0
 set_param general.usePosixSpawnForFork 1
+set_param bd.open.in_stealth_mode 9
+set_param synth.incrementalSynthesisCache ./.Xil/Vivado-759351-emerald/incrSyn
+set_msg_config -id {Synth 8-256} -limit 10000
+set_msg_config -id {Synth 8-638} -limit 10000
 OPTRACE "Creating in-memory project" START { }
 create_project -in_memory -part xc7a100tfgg484-2
 
